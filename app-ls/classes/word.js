e3d6efe8c3ca541290691ab2e8fdb9a5bfb176a0
Bruzaca <fbruzacac@gmail.com>
2024-09-17 18:11:12 -0300

    feat: React migration

--- app-ls/classes/word.js
+++
@@ -1,5 +0,0 @@
-export default class SortedWord {
-    static id;
-    static word;
-    static hint;
-}
--- app-ls/dataBase/preload.js
+++
@@ -1,9 +0,0 @@
-import DataBase from "./dataBase.js";
-
-async function preload(){
-    if (DataBase.notLoadedWords()){
-        DataBase.getAllWords();
-    }
-    DataBase.getTopPlayers();
-}
-export default preload;
