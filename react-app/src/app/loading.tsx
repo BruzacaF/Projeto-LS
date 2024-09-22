5fc7cc166531d387fd6a16237c1d09127d4e6b2e
Bruzaca <fbruzacac@gmail.com>
2024-09-22 19:09:27 -0300

    Fixed Bugs

--- react-app/src/app/loading.tsx
+++ react-app/src/app/loading.tsx
@@ -1,4 +1,4 @@
-
+'use client'
 // Criar um arquivo de loading para ser exibido enquanto a pagina carrega
 // Criar um arquivo de contexto para o loading (opcional)
 // Criar um arquivo de assets para as imagens do loading (opcional)
