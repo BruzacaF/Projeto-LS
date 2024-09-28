cbc047046631aa9537844d0da829d3eab55f01ca
Julielison Lima <127698034+Julielison@users.noreply.github.com>
2024-09-28 17:28:29 -0300

    add: ranking

--- react-app/src/components/nav.tsx
+++ react-app/src/components/nav.tsx
@@ -1,23 +1,28 @@
 
 import Button from "./button";
 import '@/components/css/header.css';
+import { links } from "@/components/assets/header-nav/references-links";
+
 
 // Organinizar o css do nav
 // Organizar o mapeamento dos links ver nota em header.tsx
 
 
 export default function Nav() {
-    
-    const links = ["Sobre", "Ajuda", "Regras"];
 
     return (
         <>
         <nav className="nav">
-            {links.map((link, index) => {
-                return <Button key={index} name={link} buttonSize="small" textSize="small" />
-            })}
+            {links.map((link, index) => (
+                <Button
+                    key={index}
+                    name={link.name}
+                    href={link.href}
+                    buttonSize="small"
+                    textSize="small"
+                />
+            ))}
         </nav>
-        
         </>
     );
 }
---
+++ react-app/src/services/supabase.tsx
@@ -0,0 +1,89 @@
+import { createClient, SupabaseClient } from '@supabase/supabase-js';
+
+const API_KEY: string = process.env.NEXT_PUBLIC_SUPABASE_KEY as string;
+const API_URL: string = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+
+// Cria o cliente Supabase com tipagem explícita
+const supabase: SupabaseClient = createClient(API_URL, API_KEY);
+
+
+export default class DataBase {
+
+    // Array de objetos com nome e score
+    static topPlayers: Array<{ name: string; score: number }>;
+
+    // Verifica se o usuário existe no banco de dados
+    static async userExists(userName: string): Promise<number | null> {
+        try {
+            const { data, error } = await supabase
+                .from('players')
+                .select('id')
+                .eq('name', userName)
+                .single();
+
+            if (error) {
+                throw error;
+            }
+
+            if (data !== null) {
+                return data.id;
+            }
+            return null;
+        } catch (err: any) {
+            console.error('Error checking user existence:', err.message);
+            return null;
+        }
+    }
+
+    static async authenticate(id: number, password: string): Promise<boolean | undefined> {
+        try {
+            // Busca a senha do usuário pelo ID
+            const { data, error } = await supabase
+                .from('players')
+                .select('password')
+                .eq('id', id)
+                .single();
+
+            if (error) {
+                throw error;
+            }
+
+            // Verifica se a senha passada é igual à senha no banco de dados
+            if (data.password === password) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (err: any) {
+            console.error('Error authenticating password:', err.message);
+        }
+    }
+
+    // Adiciona nome e senha do jogador no banco de dados
+    static async addPlayerToDatabase(userName: string, userPassword: string): Promise<void> {
+        try {
+            const { error } = await supabase
+                .from('players')
+                .insert([{ name: userName, password: userPassword }]);
+
+            if (error) throw error;
+        } catch (err: any) {
+            console.error('Error adding player to database:', err.message);
+        }
+    }
+
+    static async getTopPlayers() {
+      try {
+          const { data, error } = await supabase
+              .from('players')
+              .select('name, score')
+              .order('score', { ascending: false })
+              .limit(10);
+
+          if (error) throw error;
+          DataBase.topPlayers = data || [];  // Armazena os jogadores na variável da classe
+      } catch (err: any) {
+          console.error('Error retrieving top players:', err.message);
+      }
+    }
+}
